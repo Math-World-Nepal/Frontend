40fa35d2039c8723456efc34f5edb82389b92584
namasSinjali <namas22thapa@gmail.com>
2021-05-29 19:24:46 +0545

    click hamburger icon to reveal nav menu
    clip-path property used for transition. Scrolling of body is locked while menu is open

--- script/index.js
+++ script/index.js
@@ -1,1 +1,16 @@
+(function toggleNav(){
+    const navElem = document.querySelector("#nav-bar nav");
+    const hamButton = document.querySelector("#nav-bar .hamburger-icon");
 
+    hamButton.addEventListener("click", function(){
+        if(hamButton.classList.contains("active")){
+            hamButton.classList.remove("active");
+            navElem.classList.add("hidden");
+            document.body.style.overflow = null;
+        } else {
+            hamButton.classList.add("active");
+            navElem.classList.remove("hidden");
+            document.body.style.overflow = "hidden";
+        }
+    })
+})();
