0096c2824aef5186ea2ce7335c00192ea6f2c079
namasSinjali <71819493+namasSinjali@users.noreply.github.com>
2021-05-31 16:47:34 +0545

    Clicking elements other than nav closes nav

--- script/index.js
+++ script/index.js
@@ -1,16 +1,30 @@
-(function toggleNav(){
+(function navBehavior(){
     const navElem = document.querySelector("#nav-bar nav");
     const hamButton = document.querySelector("#nav-bar .hamburger-icon");
 
-    hamButton.addEventListener("click", function(){
+    hamButton.addEventListener("click", function(e){
+        e.stopPropagation();
+
         if(hamButton.classList.contains("active")){
-            hamButton.classList.remove("active");
-            navElem.classList.add("hidden");
-            document.body.style.overflow = null;
+            hideNav();
         } else {
-            hamButton.classList.add("active");
-            navElem.classList.remove("hidden");
-            document.body.style.overflow = "hidden";
+            displayNav();
         }
-    })
+    });
+
+    function hideNav(){
+        hamButton.classList.remove("active");
+        navElem.classList.add("hidden");
+        window.removeEventListener("click", handler);
+    }
+    function displayNav(){
+        hamButton.classList.add("active");
+        navElem.classList.remove("hidden");
+        window.addEventListener("click", handler);
+    }
+    function handler(e){
+        if(!e.path.some(e=>e===navElem)){
+            hideNav();
+        }
+    }
 })();
